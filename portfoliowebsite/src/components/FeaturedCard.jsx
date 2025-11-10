09890508979c51c7633ed7db786780fb66b471bb
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-10 18:30:08 +0200

    feat: add Featured projects section

--- portfoliowebsite/src/components/FeaturedCard.jsx
+++ portfoliowebsite/src/components/FeaturedCard.jsx
@@ -6,8 +6,8 @@ import redbg from "../assets/redbg.svg"
 const FeaturedCard = (props) => {
     return ( 
         <div className="project1">
-            <img src={redbg} alt="domtybg" className='projectBg'/>
-            <img src={domty} alt="domtyimg" />
+            <img src={props.projectbgimg} alt="domtybg" className='projectBg'/>
+            <img src={props.projectimg} alt="domtyimg" />
         <div className="projectDesDiv">
             <h4 className="projectTitle">{props.projectTitlename}</h4>
             <p className="projectDescription">{props.projectDescription}</p>
--- portfoliowebsite/src/pages/Home.jsx
+++ portfoliowebsite/src/pages/Home.jsx
@@ -5,6 +5,16 @@ import Header from '../components/Header';
 import SectionTitle from '../components/SectionTitle';
 import SectionSubtitle from '../components/SectionSubtitle';
 import FeaturedCard from '../components/FeaturedCard';
+import domty from "../assets/domtymockup.svg"
+import redbg from "../assets/redbg.svg";
+import purpbg from "../assets/purp.svg"
+import art from "../assets/artmento.svg"
+import p2bg from "../assets/storybg.svg"
+import story from "../assets/lap.svg";
+import arrowdiv from "../assets/arrowdiv.svg"
+
+
+
 const Home = () => {
     return ( 
         <>
@@ -16,11 +26,28 @@ const Home = () => {
 
             <SectionTitle sectiontitlename="Featured Projects"/>
             <SectionSubtitle sectionsubtitlename="Projects that best capture my approach to creating meaningful user experiences."/>
+                    <img src={arrowdiv} alt="" class="animate"/>
+
             </div>
             <div className='for3projects'>
                 <FeaturedCard
                 projectTitlename="Domty Rebranding"
+                projectbgimg={redbg}
+                                projectimg={domty}
+
                 projectDescription="A rebranding of the well-known Egyptian company Domty, featuring interactive components and stunning visuals, and a modern and intuitive interface"
+                />
+                  <FeaturedCard
+                projectTitlename="Art Mentor App"
+                  projectbgimg={purpbg}
+                                projectimg={art}
+                projectDescription="An engaging app that helps designers start with their design thinking process such as generate ideas, create mood board, receive feedback and more."
+                />
+                  <FeaturedCard
+                projectTitlename="Story Website"
+                  projectbgimg={p2bg}
+                                projectimg={story}
+                projectDescription="A 3D interactive story-telling website that immerses users into a captivating experience.  "
                 />
             </div>
 
