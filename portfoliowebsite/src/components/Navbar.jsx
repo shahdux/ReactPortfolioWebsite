bd2ee5ee0985a79b9c0d13a4171b1ac2ef67b825
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-11 23:26:46 +0200

    import link to works page

--- portfoliowebsite/src/components/Navbar.jsx
+++ portfoliowebsite/src/components/Navbar.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import "./Navbar.css"
 import NavLinkTitle from './NavLinkTitle';
 import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <div className='navbar'>
             <img src={logo} alt="" className='logo'/>
               <div class="forlinks">
                 <NavLinkTitle NavlinkTitle="Services"/>
-                <NavLinkTitle NavlinkTitle="Works"/>
+                <Link to="/works">   <NavLinkTitle NavlinkTitle="Works"/></Link> 
                                 <NavLinkTitle NavlinkTitle="Blog"/>
                 <NavLinkTitle NavlinkTitle="About Us"/>
                 <NavLinkTitle NavlinkTitle="Contact Us"/>
