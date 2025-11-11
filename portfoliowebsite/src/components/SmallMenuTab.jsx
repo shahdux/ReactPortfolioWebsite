4952de0a36feff2221df1a42bf28de25cfb2f1d3
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-11 23:52:42 +0200

    feat: add Small MenuTab component

---
+++ portfoliowebsite/src/components/SmallMenuTab.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import "./SmallMenuTab.css";
+const SmallMenuTab = (props) => {
+    return ( <>
+        <p className='menutabtitle' >{props.menutabtitle}</p>
+         <p className='bluemenutabtitle' >{props.menutabtitleblue}</p>
+    </>
+     );
+}
+ 
+export default SmallMenuTab;
--- portfoliowebsite/src/pages/Works.jsx
+++ portfoliowebsite/src/pages/Works.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import "./Works.css"
+import Navbar from '../components/Navbar';
+import imgtitle from "../assets/workss.svg"
+import SmallMenuTab from '../components/SmallMenuTab';
 const Works = () => {
     return (
         <>
+        <Navbar/>
         
-        
-        
+              <img class="heads" src={imgtitle}/>
+              <SmallMenuTab menutabtitleblue="UX/UI"/>
+             
+
         
         
         
