e97506c9e45e904ef28afd6c6447f8bd7c3fe2c2
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-13 01:08:39 +0200

    style: add animations in project Details page

--- portfoliowebsite/src/pages/ProjectDetails.jsx
+++ portfoliowebsite/src/pages/ProjectDetails.jsx
@@ -17,6 +17,7 @@ import SubscriptionSection from '../components/SubscriptionSection';
 import Footer from '../components/Footer';
 import CircularText from '../components/CircularText ';
 import arrowdivwhite from "../assets/whitearrowspin.svg";
+import RotatingText from '../components/RotatingText'
 
 
 
@@ -30,8 +31,20 @@ const ProjectDetails = () => {
 
         <div className="mockupText">
 <p className="mockTitle">Your all in one</p>
-<p className="mockfeature">Moodboard generator
-</p>
+{/* <p className="mockfeature">Moodboard generator
+</p> */}
+<RotatingText
+  texts={['Moodboard Generator', 'Feedback provider', 'File Summarizer', 'Art Advisor']}
+  mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+  staggerFrom={"last"}
+initial={{ y: "100%", opacity: 0 }}
+animate={{ y: 0, opacity: 1 }}
+exit={{ y: "100%", opacity: 0 }}
+  staggerDuration={0.025}
+  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+  transition={{ type: "spring", damping: 30, stiffness: 400 }}
+  rotationInterval={2000}
+/>
 
 
 <p className="mockupdes">ArtMento helps you with any design-related challenges </p>
