c5afc32ed705c50ee429c85d226371216647c0e3
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-27 11:42:42 +0200

    feat: add blogs page

--- portfoliowebsite/src/pages/Blog.jsx
+++ portfoliowebsite/src/pages/Blog.jsx
@@ -3,14 +3,24 @@ import "./Blog.css"
 import Navbar from '../components/Navbar';
 import Blogimg from '../assets/blogimg.svg';
 import blogimg1 from '../assets/blogimg1.svg';
-// import bgblur from '../assets/bgblur.png';
+import blogimg2 from '../assets/blogimg2.svg';
+
+import {Helmet} from "react-helmet";
 
 
 import BlogCard from '../components/BlogCard';
+import SubscriptionSection from '../components/SubscriptionSection';
+import Footer from '../components/Footer';
 
 const Blog = () => {
+
     return ( 
         <>
+            <Helmet>
+                <title>Blog</title>
+                <meta name="description" content="jhjkhkh" />
+                  <link rel="icon" type="image/png" href="/smalllogo.png" sizes="16x16" />    
+            </Helmet>
         
         <Navbar/>
       <img class="heads" src={Blogimg} alt='blog title image'/>
@@ -18,10 +28,13 @@ const Blog = () => {
                 <BlogCard blogtitle="How I helped mothers manage their cooking ingredients"
                 blogdes="How a UX UI app helped mothers why struggle with remembering the ingredients when deciding to prep for a meal at home."
                 blogsimg={blogimg1}/>
+                   <BlogCard blogtitle="Top Qualities of a Professional UI Designer in Egypt"
+                blogdes="There are some important qualities every UI designer in Egypt should have. Plently of UI designers in Egypt have a unique way in producing there work."
+                blogsimg={blogimg2}/>
             </div>
-            {/* <img src={bgblur} alt="bg blur" className='bgblurstyle' /> */}
 
-        
+        <SubscriptionSection/>
+        <Footer/>
         
         </>
      );
