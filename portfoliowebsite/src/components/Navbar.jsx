46ad564d330f8c0efd231dcfc4f8974dded3ad95
shahdMohammad <shahdmohammadx@gmail.com>
2025-11-28 21:37:28 +0200

    style: add responsiveness to nabar

--- portfoliowebsite/src/components/Navbar.jsx
+++ portfoliowebsite/src/components/Navbar.jsx
@@ -1,32 +1,51 @@
-import React, { Component } from 'react';
-import "./Navbar.css"
+
+import React, { useState } from 'react';
+import "./Navbar.css";
 import NavLinkTitle from './NavLinkTitle';
 import logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return ( 
-        <div className='navbar'>
-           <Link to="/"> 
-            <img src={logo} alt="" className='logo'/></Link> 
-              <div className="forlinks">
-                <Link to="/works" className="no-underline"><NavLinkTitle NavlinkTitle="Works"/></Link> 
-                <Link to="/blogs" className="no-underline">
-                                <NavLinkTitle NavlinkTitle="Blog"/>
-                </Link>
-                                <Link to="/about" className="no-underline">
-                <NavLinkTitle NavlinkTitle="About Us"/>
-                                </Link>
-                                                                <Link to="/contact" className="no-underline">
+  const [menuOpen, setMenuOpen] = useState(false);
 
-                <NavLinkTitle NavlinkTitle="Contact Us"/></Link>
+  return ( 
+    <div className='navbar'>
+      
+      <Link to="/"> 
+        <img src={logo} alt="" className='logo' />
+      </Link> 
 
-       
-        
-        
+      
+      <div 
+        className="burger" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+     
+      <div className={`forlinks ${menuOpen ? "open" : ""}`}>
+        <Link to="/works" className="no-underline">
+          <NavLinkTitle NavlinkTitle="Works"/>
+        </Link> 
+
+        <Link to="/blogs" className="no-underline">
+          <NavLinkTitle NavlinkTitle="Blog"/>
+        </Link>
+
+        <Link to="/about" className="no-underline">
+          <NavLinkTitle NavlinkTitle="About Us"/>
+        </Link>
+
+        <Link to="/contact" className="no-underline">
+          <NavLinkTitle NavlinkTitle="Contact Us"/>
+        </Link>
+      </div>
 
     </div>
-        </div>
-     );
+  );
 }
  
 export default Navbar;
