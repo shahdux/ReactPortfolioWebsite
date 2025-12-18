3ec578c38c5a76397a591639e37e8846c2b958c3
shahdMohammad <shahdmohammadx@gmail.com>
2025-12-19 01:03:00 +0200

    feat: add APIs to Subscription section

--- portfoliowebsite/src/components/SubscriptionSection.jsx
+++ portfoliowebsite/src/components/SubscriptionSection.jsx
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import "./SubscriptionSection.css"
 import Strokebutton from './Strokebutton';
 import buttonArrow from "../assets/whitearrow.svg"
+import { supabase } from '../Supabase';
 
 
 
 const SubscriptionSection = () => {
+    
+        const [loading, setLoading] = useState(true);
+                const [sub, setSub] = useState("");
+          
+                useEffect(()=>{
+                      async function callGetAPIs2(){
+                            const res = await supabase.from("Subscribtion").select("*");
+                            setSub(res.data);
+                            // console.log(res);
+                            setLoading(false);
+                      }
+                      callGetAPIs2();
+                },[]);
+                if (loading) return <p>Loading...</p>;
     return ( 
         <>
         
-        
-       <div className="letsWorkdiv">
-    <p className="letsTitle">Lets Work Together</p>
-     <p className="letsDes">I'm a dedicated UX/UI designer with a passion for crafting user-centered designs. My approach combines creativity and analytical thinking to deliver solutions that resonate with users. Together, we can transform your ideas into engaging and functional experiences. Let’s collaborate to create something truly impactful!</p>
+           {
+           sub.map((subs)=>{
+             return     <div className="letsWorkdiv">
+    <p className="letsTitle">{subs.title}</p>
+     <p className="letsDes">{subs.description}</p>
      <div className="for2buttons">
         <input className="inputc" type="text" name="email" placeholder="Your email address"/>
         {/* <input class="inputc" type="text" name="email" placeholder="Your email address"> */}
@@ -21,6 +37,9 @@ const SubscriptionSection = () => {
            />
      </div>
 </div>
+            })
+          }
+      
         </>
      );
 }
